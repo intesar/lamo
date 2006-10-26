339cbc9613bde7ec4da26a1e958d73e8940df4ec
mdshannan <mdshannan@ec031542-7b1f-0410-bc1d-8998f0bbda0f>
2006-10-26 06:23:58 +0000

    dfd

---
+++ src/hello/Main.java
@@ -0,0 +1,33 @@
+/*
+ * Main.java
+ *
+ * Created on August 18, 2006, 2:53 AM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package hello;
+
+/**
+ *
+ * @author home
+ */
+public class Main {
+    
+    /** Creates a new instance of Main */
+    public Main() {
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Employee e1 = new Employee(100);
+        e1.setAge(22);
+        e1.setName("apple");
+        e1.setSalary(343.3);
+        System.out.println("salary : " + e1.getID());
+    }
+    
+}
